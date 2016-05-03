4792b1b4c422729be6df8c484b79df54b1eec4db
Lucas <llago@thoughtworks.com>
2016-05-03 11:47:59 -0300

    refactor: rename comment components folder

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -2,8 +2,8 @@ import React from 'react';
 import {render} from 'react-dom';
 import './css/base.css';
 
-import CommentBox from './js/commentbox/commentBox';
+import CommentBox from './js/comments/commentBox';
 
 render(
-  <CommentBox url='/api/comments' pollInterval={1000}/>, document.getElementById('content')
+  <CommentBox url="/api/comments" pollInterval={1000}/>, document.getElementById('content')
 );
